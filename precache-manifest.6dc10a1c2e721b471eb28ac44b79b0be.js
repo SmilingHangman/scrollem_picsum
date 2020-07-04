edff46df4478fede2f2a08fad79e6905fbe6231f
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-07-04 20:40:31 +0300

    Updates

---
+++ precache-manifest.6dc10a1c2e721b471eb28ac44b79b0be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916ec4ac9c0f80753a2cae0e65e3e9eb",
+    "url": "/scrollem_picsum/index.html"
+  },
+  {
+    "revision": "a475d10f6b39c044dd1f",
+    "url": "/scrollem_picsum/static/css/main.03fe0246.chunk.css"
+  },
+  {
+    "revision": "9b9159163cb7a73e251b",
+    "url": "/scrollem_picsum/static/js/2.09dec255.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scrollem_picsum/static/js/2.09dec255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a475d10f6b39c044dd1f",
+    "url": "/scrollem_picsum/static/js/main.f8d36769.chunk.js"
+  },
+  {
+    "revision": "9d8d717a86dd072b3a77",
+    "url": "/scrollem_picsum/static/js/runtime-main.1d436d2b.js"
+  }
+]);
